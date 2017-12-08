1fe82aa18b9cd55e507b6ef19bc9c17ac48fc3f7
Sophie Chhoeu <sophie.chhoeu@gmail.com>
2017-12-08 18:35:59 +1100

    checking if commits exist and are valid comments then render

--- react-web/src/components/Movie.js
+++ react-web/src/components/Movie.js
@@ -9,6 +9,53 @@ export default function Movie({
   director,
   comments
 }) {
+
+  /*
+   * Validate the comments props passed down from parent container component.
+   * First check that comments is not of type `undefined` (i.e. `typeof comments != undefined`)
+   * Secondly if it is defined (i.e. truthy), then we'll make sure it's not an empty array
+   * (i.e. NOT [], as we want [{title: "", _id: ""}])
+   */
+  function isValidComments(comments) {
+    return comments && comments.length > 0
+  }
+
+  /*
+   * Assuming the comments props are valid, we'll only display the comments component if
+   * the comments prop that is passed down if it has an `_id` property
+   */
+  function showValidComments(comments) {
+    return comments
+    .filter(comment => {
+      return comment.hasOwnProperty('_id');
+    })
+    .map(comment => {
+      return (
+        <Comment key={comment._id}>
+          {comment.body}
+        </Comment>
+      )
+      //return comment
+    })
+  }
+
+  // the container MovieList component is rendering multiple Movies (i.e. an outer array
+  // of Movies with comments).
+  // each time it renders a Movie, it sends down the `props`.
+  // the `props` contain `comments` (i.e. this.props.comments),
+  // we've used destructuring to extract the `comments`
+  // but the prop `comments` is actually an array itself (i.e. [] or [{title: "", _id: ""}])
+  // so we only want to show those that aren't empty [], so we use a conditional check
+  // i.e. `comments.length > 0`, then we also want to make sure it contains a hash with
+  // the properties we're after (i.e. _id)
+
+  // debugging only...
+  // if (comments.length > 0) {
+  //   console.log(showComments(comments))
+  // }
+
+  console.log(comments)
+
   return (
     <div>
       <span>Title: {title}</span>
@@ -25,15 +72,8 @@ export default function Movie({
     </p>
       <span>
         <p>
-        Comments: {
-          comments.length ? (
-            comments.map(comment => (
-              <Comment key={comment._id}>
-              {comment.body}
-              </Comment>
-            ))
-
-          ) :   ('no comments')
+        comments: {
+          isValidComments(comments) ? showValidComments(comments) : 'no comments'
         }
       </p>
       &nbsp;
--- react-web/src/components/MovieList.js
+++ react-web/src/components/MovieList.js
@@ -2,12 +2,13 @@ import React from 'react';
 import Movie from './Movie'
 
 export default function Movielist({ movies }) {
+  console.log(movies)
   return (
     <div>
       <h1>Movie List!</h1>
       {
         movies.map(movie => {
-        return <Movie {...movie} key={movie._id} />
+          return <Movie {...movie} key={movie._id} />
         })
       }
     </div>
